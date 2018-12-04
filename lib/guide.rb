d944e14ce6522a467231d12dee32937d7a2e67df
Jay Ortega <jfortega35@gmail.com>
2018-12-04 14:46:56 -0500

    Added support folder with number and string formatting classes/modules.  Improving output video

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,4 +1,5 @@
 require 'restaurant'
+require 'support/string_extend'
 
 class Guide
 
@@ -61,18 +62,14 @@ class Guide
   end
 
   def list
-    puts "\nListing restaurants\n\n".upcase
+    output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
-    restaurants.each do |rest|
-      puts rest.name + " | " + rest.cuisine + " | " + rest.price
-    end
+    output_restaurant_table(restaurants)
   end
 
   def add
-    puts "\nAdd a restaurant\n\n".upcase
-
+    output_action_header("\nAdd a restaurant\n\n")
     restaurant = Restaurant.build_using_questions
-
     if restaurant.save
       puts "\nRestaurant Added\n\n"
     else
@@ -88,4 +85,25 @@ class Guide
   def conclusion
     puts "\n<<< Goodbye and Bon Apetit! >>>\n\n\n"
   end
+
+  private
+  def output_action_header(text)
+    puts "\n#{text.upcase.center(60)}\n\n".upcase
+  end
+
+  def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".ljust(6) + "\n"
+    puts "-" * 60
+    restaurants.each do |rest|
+      line = " " << rest.name.titleize.ljust(30)
+      line << " " + rest.cuisine.titleize.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+    end
+    puts "No listings found" if restaurants.empty?
+    puts "-" * 60
+  end
+
 end
